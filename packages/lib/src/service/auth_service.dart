1f786808164c7fcbdd3ba24fb701b8f24dd99a82
ytolgaarda <ty.5252@hotmail.com>
2025-02-09 22:53:38 +0300

    supabase auth and authstate changes completed

--- packages/lib/src/service/auth_service.dart
+++ packages/lib/src/service/auth_service.dart
@@ -19,9 +19,20 @@ class AuthService {
     return response;
   }
 
-  Future<AuthResponse> signUp(String email, String password) async {
+  Future<AuthResponse> signUp(
+      String email, String password, String name) async {
     final response =
         await _supabaseClient.auth.signUp(email: email, password: password);
+
+    final user = response.user;
+
+    if (user != null) {
+      await _saveUserToDatabase(
+          UserModel(id: user.id, name: name, email: email));
+    } else {
+      throw Exception("Kullanıcı oluşturulamadı.");
+    }
+
     return response;
   }
 
@@ -30,15 +41,18 @@ class AuthService {
   }
 
   Future<UserModel?> fetchMyUser() async {
-    final session = _supabaseClient.auth.currentSession;
-    final user = session?.user;
+    final user = _supabaseClient.auth.currentUser;
+    if (user == null) return null;
 
-    if (user != null) {
-      return UserModel.fromJson(user.toJson());
-    } else {
-      return null;
-    }
+    final response =
+        await _supabaseClient.from('users').select().eq('id', user.id).single();
+
+    return UserModel.fromJson(response);
   }
 
   Stream<AuthState> streamAuthState() => _supabaseClient.auth.onAuthStateChange;
+
+  Future<void> _saveUserToDatabase(UserModel user) async {
+    await _supabaseClient.from('users').insert(user.toJson());
+  }
 }
--- packages/test/packages_test.dart
+++
@@ -1,4 +0,0 @@
-import 'package:packages/packages.dart';
-import 'package:test/test.dart';
-
-void main() {}
